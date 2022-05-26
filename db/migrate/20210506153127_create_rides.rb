26d595ad8ace5d34fc25b3d679b39f8eb14b371c
Michael Franco <franco.luis.michael@gmail.com>
2022-05-25 19:01:07 -0500

    commit tree b2ce41123d2814dcb75be4c3115f759667f3f2eb

--- db/migrate/20210506153127_create_rides.rb
+++ db/migrate/20210506153127_create_rides.rb
@@ -1,6 +1,8 @@
 class CreateRides < ActiveRecord::Migration[6.1]
   def change
     create_table :rides do |t|
+      t.integer "taxi_id", null: true
+      t.integer "passenger_id", null: true
 
       t.timestamps
     end
